213d2983e45ece5f25cd2b0f211474a9f2bfd7ef
Phodal Huang <h@phodal.com>
2020-05-25 22:09:35 +0800

    styles: add more scss for render themes

--- projects/@ledge-framework/render/gulpfile.js
+++ projects/@ledge-framework/render/gulpfile.js
@@ -7,13 +7,13 @@ var sass = require('gulp-sass');
 sass.compiler = require('sass');
 
 gulp.task('sass', function () {
-  return gulp.src('./src/lib/theme/*.scss')
+  return gulp.src('./src/lib/theme/index.scss')
     .pipe(sass({fiber: Fiber}).on('error', sass.logError))
     .pipe(gulp.dest('./prebuild-theme'));
 });
 
 gulp.task('sass:watch', () => {
-  gulp.watch('./src/lib/theme/*.scss', (done) => {
+  gulp.watch('./src/lib/theme/index.scss', (done) => {
       gulp.series(['sass'])(done);
   });
 });
